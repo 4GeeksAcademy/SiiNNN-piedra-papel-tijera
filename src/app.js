fab4a0e38da56e7d0f0fdd1862f1d403e13e5ae6
eudald <eudaldpoulozano@gmail.com>
2026-06-17 19:27:36 +0000

    piedra papel tijera

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-//write your code here
-console.log("Hello Rigo from the console!");
 
+function jugar (){
+const opciones = ["piedra", "papel", "tijera","lagarto","spock"]
+const opcionespc = opciones[Math.floor(Math.random() * opciones.length)]
+let elecop = "piedra";
+const reglas = {
+    piedra : ["tijera", "lagarto"],
+    papel : ["piedra", " spock"],
+    tijera : [ "papel", " lagarto"],
+    lagarto : ["papel", "spock"],
+    spock : ["piedra", "tijera"],
+}
+console.log(`El jugador eligió ${elecop}`);
+console.log(`La maquina eligió ${opcionespc}`);
+console.log(`El resultado es .....`);   
+if (elecop === opcionespc){
+    console.log("Ha habido un empate!");
+}
+else if ( reglas[elecop].includes(opcionespc)){
+    console.log("Tu Ganas");
+}
+else 
+{console.log("Eres un perdedor!!!");
+}
+}
+jugar();
